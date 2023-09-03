149e8e6f6f8340690d3251bb1e2947018f269f19
paplauskis <paplauskaskostas@gmail.com>
2023-09-03 22:17:27 +0300

    coded Start-window code

---
+++ src/components/start-window/Start-window.jsx
@@ -0,0 +1,27 @@
+import './Start-window.css'
+import { useState } from 'react'
+
+function StartWindow() {
+  const [show, setShow] = useState(true)
+
+  function handleClick() {
+    setShow(false)
+  }
+  return (
+    <>
+      {show && (
+        <div className="background-div">
+          <div className="start-window">
+            <h2 className="instructions">Instructions</h2>
+            <p className="description">
+              Don't click on the same card twice. That's about it. Good luck!
+            </p>
+            <button onClick={handleClick}>Let's go!</button>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default StartWindow
