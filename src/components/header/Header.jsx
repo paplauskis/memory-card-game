556a0b348995939597fc1bebd3f352589e05e66e
paplauskis <paplauskaskostas@gmail.com>
2023-09-04 20:54:20 +0300

    added props to child components and imported CSS file

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,15 @@
-import BestScore from "./scores/Best-score"
-import CurrentScore from "./scores/Current-score"
+import BestScore from './scores/Best-score'
+import CurrentScore from './scores/Current-score'
+import './Header.css'
 
-function Header() {
+function Header({currentScore, bestScore}) {
+  
   return (
     <header>
       <h1>Memory Card Game</h1>
       <div className="scores">
-      <BestScore/>
-      <CurrentScore/>
+        <BestScore bestScore={bestScore} />
+        <CurrentScore currentScore={currentScore} />
       </div>
     </header>
   )
