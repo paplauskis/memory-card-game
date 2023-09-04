7ce85fabfa79172b6c911bf7f68a85f27e54f0fd
paplauskis <paplauskaskostas@gmail.com>
2023-09-04 17:33:26 +0300

    added useEffect that fetches pokemons from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
-
-import './App.css'
+import Header from './components/header/Header'
+import StartWindow from './components/start-window/Start-window'
+import Cards from './components/cards/Cards'
+import { useState, useEffect } from 'react'
 
 function App() {
-
+  const [pokemon, setPokemon] = useState([])
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setPokemon(data.results)
+      })
+      .catch((error) => {
+        console.log('Error fetching data:', error)
+      })
+  }, [])
 
   return (
     <>
+      <Header />
+      <StartWindow />
+      <Cards pokemon={pokemon}/>
     </>
   )
 }
