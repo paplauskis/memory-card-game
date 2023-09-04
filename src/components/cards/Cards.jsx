848168baf2c7afb78e65d87c29ebf80b988c0d92
paplauskis <paplauskaskostas@gmail.com>
2023-09-04 17:36:08 +0300

    redone Cards component and added getPokemonImg function
    getPokemonImg fetches image from API and returns required url. Cards maps over pokemon names and creates a div

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,30 +1,33 @@
 import './Cards.css'
 import { useEffect, useState } from 'react'
 
-function Cards() {
-  const [pokemon, setPokemon] = useState([])
+function getPokemonImg(url) {
+  const [pokemonUrl, setPokemonUrl] = useState(null)
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemons[11]}`)
+    fetch(url)
       .then((resp) => resp.json())
-      .then((data) => {
-        console.log(data)
-      })
-  })
+      .then((data) => setPokemonUrl(data.sprites.other['official-artwork'].front_default))
+      .catch((error) => console.log('Error, could not load pokemon image:', error))
+  }, [])
+
+  return pokemonUrl
 }
 
-const pokemons = [
-  'rattata',
-  'raticate',
-  'pidgeot',
-  'pidgey',
-  'beedrill',
-  'kakuna',
-  'weedle',
-  'butterfree',
-  'metapod',
-  'caterpie',
-  'blastoise',
-  'charizard'
-] 
+function Cards({ pokemon }) {
+  return (
+    <div className="cards">
+      {pokemon.map((p, index = 0) => {
+        if (++index <= 12) {
+          return (
+            <div className="card" key={p.name}>
+              <img src={getPokemonImg(p.url)} alt="pokemon" />
+              <p>{p.name.toUpperCase()}</p>
+            </div>
+          )
+        }
+      })}
+    </div>
+  )
+}
 
 export default Cards
