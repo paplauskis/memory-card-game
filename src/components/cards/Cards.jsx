3a5e49693abcf6bd1d4d8b960120a67b96b4b3ee
paplauskis <paplauskaskostas@gmail.com>
2023-09-04 20:52:14 +0300

    added function that shuffles pokemon array and event listeners to divs

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -6,20 +6,44 @@ function getPokemonImg(url) {
   useEffect(() => {
     fetch(url)
       .then((resp) => resp.json())
-      .then((data) => setPokemonUrl(data.sprites.other['official-artwork'].front_default))
-      .catch((error) => console.log('Error, could not load pokemon image:', error))
+      .then((data) =>
+        setPokemonUrl(data.sprites.other['official-artwork'].front_default)
+      )
+      .catch((error) =>
+        console.log('Error, could not load pokemon image:', error)
+      )
   }, [])
 
   return pokemonUrl
 }
 
+function shufflePokemons(array) {
+  let al = array.length,
+    randomNum,
+    i
+
+  while (al) {
+    randomNum = Math.floor(Math.random() * al--)
+    i = array[al]
+    array[al] = array[randomNum]
+    array[randomNum] = i
+  }
+  return array
+}
+
 function Cards({ pokemon }) {
+  shufflePokemons(pokemon)
+
+  function handleCardClick(e) {
+    e.target.value = 'clicked'
+  }
+
   return (
     <div className="cards">
       {pokemon.map((p, index = 0) => {
         if (++index <= 12) {
           return (
-            <div className="card" key={p.name}>
+            <div className="card" key={p.name} onClick={handleCardClick}>
               <img src={getPokemonImg(p.url)} alt={`${p.name}, pokemon`} />
               <p>{p.name.toUpperCase()}</p>
             </div>
