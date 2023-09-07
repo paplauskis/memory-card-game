f1353201dbb2587b4116420ec463868b78c3206b
paplauskis <paplauskaskostas@gmail.com>
2023-09-07 14:45:28 +0300

    removed fetch from API, added array with info instead
    it was removed due to a bug that does not render  my .card divs on page load, I could not resolve the issue so I just removed the useEffect hook that fetches pokemon array

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,30 @@ import StartWindow from './components/start-window/Start-window'
 import Cards from './components/cards/Cards'
 import { useState, useEffect } from 'react'
 
+const arr = [
+  { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
+  { name: 'ivysaur', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
+  { name: 'venusaur', url: 'https://pokeapi.co/api/v2/pokemon/3/' },
+  { name: 'charmander', url: 'https://pokeapi.co/api/v2/pokemon/4/' },
+  { name: 'charmeleon', url: 'https://pokeapi.co/api/v2/pokemon/5/' },
+  { name: 'charizard', url: 'https://pokeapi.co/api/v2/pokemon/6/' },
+  { name: 'squirtle', url: 'https://pokeapi.co/api/v2/pokemon/7/' },
+  { name: 'wartortle', url: 'https://pokeapi.co/api/v2/pokemon/8/' },
+  { name: 'blastoise', url: 'https://pokeapi.co/api/v2/pokemon/9/' },
+  { name: 'caterpie', url: 'https://pokeapi.co/api/v2/pokemon/10/' },
+  { name: 'metapod', url: 'https://pokeapi.co/api/v2/pokemon/11/' },
+  { name: 'butterfree', url: 'https://pokeapi.co/api/v2/pokemon/12/' },
+]
+
 function App() {
-  const [pokemon, setPokemon] = useState([])
   const [currentScore, setCurrentScore] = useState(0)
   const [bestScore, setBestScore] = useState(0)
-  useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon`)
-      .then((resp) => resp.json())
-      .then((data) => {
-        setPokemon(data.results.splice(0, 12))
-      })
-      .catch((error) => {
-        console.log('Error fetching data:', error)
-      })
-  }, [])
 
   return (
     <>
       <Header currentScore={currentScore} bestScore={bestScore} />
       <StartWindow />
-      <Cards pokemon={pokemon} />
+      <Cards pokemon={arr} />
     </>
   )
 }
