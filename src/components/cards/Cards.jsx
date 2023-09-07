3a122b07ec394bd5f93e311a768e79d08b020b47
paplauskis <paplauskaskostas@gmail.com>
2023-09-07 14:46:40 +0300

    reordered functions out of Cards component, made them shuffle on click

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,68 +1,58 @@
 import './Cards.css'
 import { useEffect, useState } from 'react'
 
+function getPokemonImg(url) {
+  const [pokemonUrl, setPokemonUrl] = useState(null)
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) =>
+        setPokemonUrl(data.sprites.other['official-artwork'].front_default)
+      )
+      .catch((error) =>
+        console.log('Error, could not load pokemon image:', error)
+      )
+  }, [url])
+
+  return pokemonUrl
+}
+
+function shuffleArray(array) {
+  let al = array.length,
+    randomNum,
+    i
+
+  while (al) {
+    randomNum = Math.floor(Math.random() * al--)
+    i = array[al]
+    array[al] = array[randomNum]
+    array[randomNum] = i
+  }
+  return array
+}
+
 function Cards({ pokemon }) {
-  const cards = createCards({ pokemon })
-  // const [cardData, setCardData] = useState(cards)
-
-  function getPokemonImg(url) {
-    const [pokemonUrl, setPokemonUrl] = useState(null)
-    useEffect(() => {
-      fetch(url)
-        .then((resp) => resp.json())
-        .then((data) =>
-          setPokemonUrl(data.sprites.other['official-artwork'].front_default)
-        )
-        .catch((error) =>
-          console.log('Error, could not load pokemon image:', error)
-        )
-    }, [url])
-
-    return pokemonUrl
-  }
-
-  function shuffleArray(array) {
-    let al = array.length,
-      randomNum,
-      i
-
-    while (al) {
-      randomNum = Math.floor(Math.random() * al--)
-      i = array[al]
-      array[al] = array[randomNum]
-      array[randomNum] = i
-    }
-    return array
-  }
-
-  function createCards({ pokemon }) {
-    const cardDivs = []
-
-    pokemon.forEach((p, index = 0) => {
-      if (++index <= 12) {
-        cardDivs.push(
-          <div className="card" key={p.name}>
-            <img src={getPokemonImg(p.url)} alt={`${p.name}, pokemon`} />
-            <p>{p.name.toUpperCase()}</p>
-          </div>
-        )
-      }
-    })
-
-    return cardDivs
-  }
+  // Create state to store the shuffled cards
+  const [shuffledCards, setShuffledCards] = useState(pokemon);
 
   function handleCardClick(e) {
+    e.target.clicked ? console.log('gameover') : console.log('good click')
     e.target.clicked = true
-    console.log(e.target)
-    
+    setShuffledCards(shuffleArray([...shuffledCards]));
   }
 
   return (
     <div className="cards" onClick={handleCardClick}>
-      {cards}
+      {shuffledCards.map((p) => (
+        <div className="card" key={p.name}>
+          <img src={getPokemonImg(p.url)} alt={`${p.name}, Pokémon`} />
+          <p>{p.name.toUpperCase()}</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
+
 
 export default Cards
