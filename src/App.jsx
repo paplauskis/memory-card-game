9748c032819c453e900f7a5150dac17b831dad3d
paplauskis <paplauskaskostas@gmail.com>
2023-09-07 18:59:55 +0300

    added gameOver modal when card is clicked twice

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,29 @@ import Header from './components/header/Header'
 import StartWindow from './components/start-window/Start-window'
 import Cards from './components/cards/Cards'
 import pokemonData from './utils/data'
+import GameOverWindow from './components/game-over-window/Game-over-window'
 import { useEffect, useState } from 'react'
 
 function App() {
   const [currentScore, setCurrentScore] = useState(0)
   const [bestScore, setBestScore] = useState(0)
   const [gameOver, setGameOver] = useState(false)
+  const [gameOverScore, setGameOverScore] = useState(0)
 
   function handleCardClick(clicked) {
     if (clicked) {
       setGameOver(true)
+      setGameOverScore(currentScore)
+      setCurrentScore(0)
     } else {
       setCurrentScore(currentScore + 1)
     }
   }
+
+  function handleGameOver(bool) {
+    setGameOver(bool)
+  }
+
   useEffect(() => {
     if (currentScore > bestScore) {
       setBestScore(currentScore)
@@ -26,6 +35,7 @@ function App() {
     <>
       <Header currentScore={currentScore} bestScore={bestScore} />
       <StartWindow />
+      {gameOver && <GameOverWindow score={gameOverScore} gameOver={gameOver} removeGameOver={handleGameOver}/>}
       <Cards pokemon={pokemonData} onCardClick={handleCardClick} />
     </>
   )
