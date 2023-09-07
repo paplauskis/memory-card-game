60f1d2be4f68e16f27fff66d58ce4bce62912e53
paplauskis <paplauskaskostas@gmail.com>
2023-09-07 16:26:21 +0300

    added callback function that tracks if pokemon was clicked already

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,23 @@ import Cards from './components/cards/Cards'
 import pokemonData from './utils/data'
 import { useState } from 'react'
 
-
-
 function App() {
   const [currentScore, setCurrentScore] = useState(0)
   const [bestScore, setBestScore] = useState(0)
+
+  function handleCardClick(clicked) {
+    if (clicked) {
+      console.log('second');
+    } else {
+      console.log('first');
+    }
+  }
 
   return (
     <>
       <Header currentScore={currentScore} bestScore={bestScore} />
       <StartWindow />
-      <Cards pokemon={pokemonData} />
+      <Cards pokemon={pokemonData} onCardClick={handleCardClick} />
     </>
   )
 }
