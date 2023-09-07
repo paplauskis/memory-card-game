dfcd3639dcf4ba23fd57ff1edbca271cd7aa0b5a
paplauskis <paplauskaskostas@gmail.com>
2023-09-07 14:52:16 +0300

    moved arr to separate file, renamed it to pokemonData

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import Header from './components/header/Header'
 import StartWindow from './components/start-window/Start-window'
 import Cards from './components/cards/Cards'
-import { useState, useEffect } from 'react'
+import pokemonData from './utils/data'
+import { useState } from 'react'
 
-const arr = [
-  { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
-  { name: 'ivysaur', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
-  { name: 'venusaur', url: 'https://pokeapi.co/api/v2/pokemon/3/' },
-  { name: 'charmander', url: 'https://pokeapi.co/api/v2/pokemon/4/' },
-  { name: 'charmeleon', url: 'https://pokeapi.co/api/v2/pokemon/5/' },
-  { name: 'charizard', url: 'https://pokeapi.co/api/v2/pokemon/6/' },
-  { name: 'squirtle', url: 'https://pokeapi.co/api/v2/pokemon/7/' },
-  { name: 'wartortle', url: 'https://pokeapi.co/api/v2/pokemon/8/' },
-  { name: 'blastoise', url: 'https://pokeapi.co/api/v2/pokemon/9/' },
-  { name: 'caterpie', url: 'https://pokeapi.co/api/v2/pokemon/10/' },
-  { name: 'metapod', url: 'https://pokeapi.co/api/v2/pokemon/11/' },
-  { name: 'butterfree', url: 'https://pokeapi.co/api/v2/pokemon/12/' },
-]
+
 
 function App() {
   const [currentScore, setCurrentScore] = useState(0)
@@ -26,7 +14,7 @@ function App() {
     <>
       <Header currentScore={currentScore} bestScore={bestScore} />
       <StartWindow />
-      <Cards pokemon={arr} />
+      <Cards pokemon={pokemonData} />
     </>
   )
 }
